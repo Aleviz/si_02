53442b08702c93e92402f735a6af453e927b33c5
Alejandro <alejandrorodriguezzv@gmail.com>
2019-09-11 09:42:44 -0600

    hola

--- cargo-academico/WebContent/resources/design/js/mapa.js
+++ cargo-academico/WebContent/resources/design/js/mapa.js
@@ -1,130 +1,220 @@
-
 //LAS COORDENADAS DEL MAPA
 var z = 13.7178;
 var x = 89.2046;
 var y = 16;
 
+var latitudBD = document.getElementById('latitud').textContent;
+var longitudBD = document.getElementById('longitud').textContent;
+var nombreBD = document.getElementById('facultad').textContent;
+var id = [1,2,3,4,5,6];
 
+var lat = latitudBD.match(/\d+(?:\.\d+)?/g).map(Number);
+var lon = longitudBD.match(/\d+(?:\.\d+)?/g).map(Number);
 
+var asd = JSON.parse(nombreBD);
 
- var latitudBD = document.getElementById('latitud').textContent;
- var longitudBD = document.getElementById('longitud').textContent;
- var nombreBD = document.getElementById('facultad').textContent;
- 
- 
- var lat =  latitudBD.match(/\d+(?:\.\d+)?/g).map(Number);
- var lon = longitudBD.match(/\d+(?:\.\d+)?/g).map(Number);
-
- var asd = JSON.parse(nombreBD);
- 
- console.log("NOMBRE ==== "+asd);
- console.log("LATITUD ==== "+lat);
- console.log("LONGITUD ==== "+lon)
-
+console.log("NOMBRE ==== " + asd);
+console.log("LATITUD ==== " + lat);
+console.log("LONGITUD ==== " + lon)
 
 // COORDENADAS DE LOS MARCADORES A MOSTRAR
 
-
-
-
-//PARA INSERTAR LAS COORDENADAS DEL MAPA EN EL MAPA
+// PARA INSERTAR LAS COORDENADAS DEL MAPA EN EL MAPA
 var map = L.map('mapa').setView([ z, -x ], y);
 
-
-//PARA AGREGAR LOS DERECHOS DE AUTOR AL MAPA
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-			attribution : '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// PARA AGREGAR LOS DERECHOS DE AUTOR AL MAPA
+L
+		.tileLayer(
+				'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+				{
+					attribution : '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				}).addTo(map);
 
 var x = [];
 
+
+
+
+map.on(	'click',function(e) {
+	console.log("------------------------------");
+	console.log(e);
+	console.log("------------------------------");
+	
+	console.log(e.target);
+	
+	console.log("------------------------------");
+	idm = e.target._leaflet_id;
+	})
+
 // PARA AGREGAR EL MARCADOR
 for (var i = 0; i < lat.length; i++) {
-	
-	marker = new L.marker([ lat[i], -lon[i] ]).bindPopup(asd[i])
-			.addTo(map);
-	
-	
-	//PARA AGREGARLO AL ARRAY
+	 var idd = id[i];
+var marker ={};
+	marker = new L.marker([ lat[i], -lon[i] ]).bindPopup(asd[i]).addTo(map);
+
+	// PARA AGREGARLO AL ARRAY
 	x.push(marker);
 
-	
 	console.log("********************");
-	 console.log("NOMBRE ==== "+asd[i]);
-	 console.log("LATITUD ==== "+lat[i]);
-	 console.log("LONGITUD ==== "+lon[i])
-	 console.log("********************");
-	 console.log(x);    
-	 
+	console.log("NOMBRE ==== " + asd[i]);
+	console.log("LATITUD ==== " + lat[i]);
+	console.log("LONGITUD ==== " + lon[i])
+	console.log("********************");
 
+	console.log(x[i]);
+	map.on('popupopen',function(e) {
+		console.log("hola " + e.popup._content);
+		$('#2qp').val(e.popup._content);
+		$('#myModal').modal("show");
+	})
+	
 }
 
-// PARA MODIFICAR EL MAPA
 
-$(document).ready(function() { //----------------------x1
+var confirmacion;
+var mensaje = ["El mapa se modificara, ¿continuar?",
+	"estas seguro de agregar un marcador aca, ¿continuar?",
+	"Los datos se guardaran, ¿continuar?" ];
+
+// PARA MODIFICAR EL MAPA
+$('#finalizar').click(function() {
+	var confirmar1 = confirm(mensaje[2]);
+		if (confirmar1 == true) {
+			confirmacion = false;
+																		
+			$(this).hide();
+			$('#editar').show();
+																				
+			console.log();	}						
+		
+	});
+$(document).ready(function() {
 	$.getScript("res/design/js/mapa.js");
-	$('#finalizar').hide();
 	
-	var mensaje = ["El mapa se modificara, ¿continuar?",
-		"estas seguro de agregar un marcador aca, ¿continuar?",
-		"Los datos se guardaran, ¿continuar?" ]
-	
-	var confirmacion;
+
+
+	var afirmar;
 	var lat;
 	var lon;
-	//ACTIVACION DEL BOTON EDITAR
-	$('#editar').click(function(){ // ----------------------x2
-		
-		confirmacion = confirm(mensaje[0]);
-		$(this).hide();
+	$('#finalizar').hide();
+	console.log(confirmacion);
+
+
+	$('#editar').click(function(){
+	
+		$('#editar').hide();
 		$('#finalizar').show();
-		 
-		//UN ALERT PARA VER SI EN REALIDAD QUIERE EDITAR O NO QUIERE  ------  x3
-		alert(confirmacion);
-		if(confirmacion == true){ // ----------------------------x4
-			map.on('click',function(e){ //-------------------------x5
-			lat = e.latlng.lat;
+		confirmacion = confirm(mensaje[0]);
+
+		console.log(confirmacion+ "dio click en editar");
+						
+		
+
+		
+		if (confirmacion == true) {
+			console.log("afirmo la edicion")
+			
+			map.on(	'click',function(e) {
+				idm = e.target._leaflet_id;
+			 lat = e.latlng.lat;
 			lon = e.latlng.lng;
-			
-			confirmacion = confirm(mensaje[1]);
-			
-			if(confirmacion == true){ // ---------------------------x6
+	
+			 afirmar = confirm(mensaje[1]);
+				break;
+
+			if (afirmar == true) {
+				console.log(afirmar + " se afirmo que ahi quiere el marcador")
+				confirmacion = false;
 				$('#myModal').modal("show");
 				$('#formCampus').hide();
-				$('#formFacultad').hide();
-				
-				$('#campus').click(function(){ //--------------------------x7
+				$('#formFacultad').hide();	
+				confirmacion = false;
+
+				$('#campus').click(function(){
 					$('#formCampus').show();
 					$('#formFacultad').hide();
 					$('#latitudCampus').val(lat);
 					$('#longitudCampus').val(lon);
 					
-					confirmacion = confirm(mensaje[2]);
+					confirmacion = false;
 					
-					if(confirmacion)
-				}) //---------------------------x7
-				
-			} // ----------------------------x6
-				
-				}) // ------------------------------------------------x5
-			} //---------------------------------------------------x4
+					$('#guardarC').click(function(){
+						market = L.marker([	lat,	lon ]).addTo(map);
+						confirmacion = false;
+
+					})
+					confirmacion = false;
+				});
+				confirmacion = false;
+				$('#facultade').click(function(){
+					confirmacion = false;
+
+					$('#formFacultad').show();
+					$('#formCampus').hide();	
+					$('#latitudFacultad').val(lat);
+					$('#longitudFacultad').val(lon);	
+					confirmacion = false;
 			
-		})// -------------------- x3
+					$('#guardarF').click(function(){
+						market = L.marker([	lat,	lon ]).addTo(map);	
+						confirmacion = false;
+					})
+					confirmacion = false;
+				});
+				confirmacion = false;
+			} else {
+				confirmacion = false;
+				console.log("denegado");
+			
+			}confirmacion = false;
+			
+			});confirmacion = false;
+
+		$('#finalizar').click(function() {
+			confirmacion = false;
+		var confirmar1 = confirm(mensaje[2]);
+			if (confirmar1 == true) {
+				confirmacion = false;
+																			
+				$(this).hide();
+				$('#editar').show();
+				confirmacion = false;											
+				console.log();			
+				confirmacion = false;
+				
+				
+				} else {confirmacion = false;
+																		
+					console.log("cambios no guardados");
+
+				}confirmacion = false;
+				
+			
+		});
+
+	
+		confirmacion = false;
+		console.log(confirmacion);
+
+	} else if(confirmacion == false){confirmacion = false;
+		$('#editar').show();
+		$('#finalizar').hide();
+		console.log("no acepto la edicion");confirmacion = false;
 		
-		
-	}) // ----------------------x2
-	
-	
-	
+	}
 
-
-
+		confirmacion = false;
+	})
+	
+	})
+			
+			
 // AL HACER CLICK ESTE RECONOCE DE DONDE ES
-for(var i = 0 ; i < x.length; i++){
-$(x[i]).click(function(e){
- 	console.log("hola "+e);
- 	$('#2qp').val(e);
-	$('#myModal').modal("show");
- })
-}
+
+	map.on('popupopen',function(e) {
+		console.log("hola " + e.popup._content);
+		$('#2qp').val(e.popup._content);
+		$('#myModal').modal("show");
+		$('#facultadfacultad').val(e.popup._content);
+	})
 
