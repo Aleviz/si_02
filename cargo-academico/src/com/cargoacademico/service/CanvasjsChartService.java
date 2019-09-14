ac22bf72445c69676718ea925b21db8c6fe60b4d
Francisco Amaya <francisco.2000.17.3@gmail.com>
2019-09-14 09:17:50 -0600

    cambios añadido de estadisticas

---
+++ cargo-academico/src/com/cargoacademico/service/CanvasjsChartService.java
@@ -0,0 +1,13 @@
+package com.cargoacademico.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+ 
+@Service
+public interface CanvasjsChartService {
+ 
+	List<List<Map<Object, Object>>> getCanvasjsChartData();
+ 
+}
